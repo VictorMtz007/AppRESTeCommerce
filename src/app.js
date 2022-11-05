f0b6704b8f1ee62ac58d025189a27d034b4be55b
VictorMtz007 <vimamartinezva@ittepic.edu.mx>
2022-11-05 10:58:59 -0700

    Solucion error 5.1

--- src/app.js
+++ src/app.js
@@ -4,17 +4,22 @@ import cors from 'cors';
 //imports Swagger
 //imports Routes
 //imports Middlewares
+
 //Config para variables de entorno
 import config from './config/config';
+
 //Declaramos la variable app igualandola a express
 const app = express();
+
 //Settings
 app.set('port', config.PORT);
+
 //Middlewares generales
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //Routes
 const api = config.API_URL;
 app.get(`${api}`, (req,res)=>{
@@ -27,8 +32,8 @@ app.get('/DrFIC', (req,res)=>{
         `<h1>RESTful running in DrFIC</h1> <p> eCommerce: <b>${api}/api-docs</b> for more information.</p>`
     );
 })
-// Routes
-// Swagger Docs
-// Middleware para el manejo de errores
-// Export App
+//Routes
+//Swagger Docs
+//Middleware para el manejo de errores
+//Export App
 export default app;
